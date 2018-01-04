df3c7bbae59753005ccb835ed8765f32116b1d45
giovanna096 <32001371+giovanna096@users.noreply.github.com>
2018-01-04 18:49:25 +0100

    Add files via upload

---
+++ BridgeReadTask.java
@@ -0,0 +1,9 @@
+package io.openems.api.bridge;
+/**
+ *
+ * @author FENECON GmbH
+ *
+ */
+public interface BridgeReadTask extends BridgeTask {
+
+}
---
+++ SessionData.java
@@ -0,0 +1,11 @@
+package io.openems.common.session;
+
+import com.google.gson.JsonObject;
+/**
+ *
+ * @author FENECON GmbH
+ *
+ */
+public interface SessionData {
+	public JsonObject toJsonObject();
+}
